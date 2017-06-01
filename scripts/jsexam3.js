ce4f970e6f93427e2dfcee66e1c75f6f1d705f5a
Bency Chacko <bencychacko@hotmail.com>
2017-06-01 10:53:41 -0400

    java

--- scripts/jsexam3.js
+++ scripts/jsexam3.js
@@ -1,12 +1,12 @@
 var myImage = document.querySelector ('img');
 myImage.onclick = function() {
 	var mySrc = myImage.getAttribute('src');
-	if (mySrc === 'images/C:/Users/bency/Desktop/pitch1.jpeg'){
+	if (mySrc === 'pitch1.jpeg'){
 
-		myImage.setAttribute ('src','images/C:/Users/bency/Desktop/pitch.jpeg');
+		myImage.setAttribute ('src','pitch.jpeg');
 
 	} else {
-		myImage.setAttribute('src', 'images/C:/Users/bency/Desktop/pitch1.jpeg');
+		myImage.setAttribute('src', 'pitch1.jpeg');
 	}
 
 }
